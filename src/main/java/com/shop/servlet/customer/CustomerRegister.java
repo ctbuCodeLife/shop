f873f2535615e83b9e532a1340ab1e5d04ef0e71
Lt-grint <1598552894@qq.com>
2017-04-24 23:43:12 +0800

    修复注册跳转页面bug

--- src/main/java/com/shop/servlet/customer/CustomerRegister.java
+++ src/main/java/com/shop/servlet/customer/CustomerRegister.java
@@ -1,10 +1,13 @@
 package com.shop.servlet.customer;
 
+import com.shop.dao.CustomerDao;
+import com.shop.domain.Customer;
 import com.shop.utils.DBUtil;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.PrintWriter;
 
 public class CustomerRegister extends HttpServlet {
@@ -18,27 +21,40 @@ public class CustomerRegister extends HttpServlet {
 	}
 
 	public void doPost(HttpServletRequest request, HttpServletResponse response) {
-		try {
-			response.setContentType("text/xml;charset=utf-8");
-			PrintWriter out = response.getWriter();
-			DBUtil db = new DBUtil();
+		try{
 			String username = request.getParameter("username");
 			String password = request.getParameter("password");
 			String tel = request.getParameter("tel");
 
-			
+			CustomerDao cd = new CustomerDao();
+
+			//提示信息的页面
+			String redirectPage = "info.jsp";
+			//自动跳转目录
+			String autoReturnPage ="index.jsp";
+			//获取session
+			HttpSession session = request.getSession();
+			//返回的提示信息
+			String msg="";
+
 			String sql = "insert into customer(username,password,tel,sex)values(?,?,?,?)";
 			Object[] params = { username, password, tel, "保密" };
-			
+			DBUtil db = new DBUtil();
 			int n = db.doUpdate(sql, params);
 			System.out.println(n);
 			if (n > 0) {
-				response.sendRedirect("login.jsp");
+				//注册成功
+				msg = "注册成功,3秒钟自动跳转到登录页面";
+				autoReturnPage = "login.jsp";
 			} else {
-				out.println("<script>alert('注册失败,该用户名已存在。')</script>");
+				//注册失败,该用户已存在
+				msg = "注册失败,3秒后自动跳转到注册页面";
+				autoReturnPage = "register.jsp";
 			}
-			out.close();
-		} catch (Exception e) {
+			session.setAttribute("msg",msg);
+			session.setAttribute("autoReturn",autoReturnPage);
+			response.sendRedirect(redirectPage);
+		}catch(Exception e){
 			e.printStackTrace();
 		}
 	}
