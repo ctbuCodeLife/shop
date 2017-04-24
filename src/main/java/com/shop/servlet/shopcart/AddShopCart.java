431cbd52be0b300e94e11c1045f70cfeadfebf9c
Lt-grint <1598552894@qq.com>
2017-04-24 13:55:16 +0800

    购物车功能完成，不再编辑

--- src/main/java/com/shop/servlet/shopcart/AddShopCart.java
+++ src/main/java/com/shop/servlet/shopcart/AddShopCart.java
@@ -1,6 +1,7 @@
 package com.shop.servlet.shopcart;
 
 import com.shop.dao.ProductDao;
+import com.shop.domain.Customer;
 import com.shop.utils.DBUtil;
 import com.shop.domain.Product;
 import com.shop.domain.ShopCart;
@@ -20,52 +21,53 @@ public class AddShopCart extends HttpServlet {
         super();
     }
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		try{
-			response.getWriter();
-			String cid = request.getParameter("cId");
-			int pId = Integer.parseInt(request.getParameter("pId"));
-			int count = Integer.parseInt(request.getParameter("count"));
-			String isBuy = request.getParameter("isBuy");
-			
-			//计算总价
-			BigDecimal totolPrice ;
-			BigDecimal unitPrice;//商品单价
-			ProductDao pd = new ProductDao();
-			Product p = new Product();
-			p = pd.find(pId);
-			unitPrice = p.getiPrice();
-			totolPrice = unitPrice.multiply(new BigDecimal(Integer.valueOf(count)));
-			//将porduct设置到session中
-            request.getSession().setAttribute("product", p);
-			//提示信息的页面
-			String redirectPage = "info.jsp";
-			//自动跳转目录
-			String autoReturnPage ="";
-			//获取session
-			HttpSession session = request.getSession();
-			//返回的提示信息
-			String msg="";
-			if(cid == null){
-				 msg="您还没有登录沃,请登录后再添加购物车吧.";
-				 autoReturnPage = "login.jsp";
-			}
-			if(cid != null && !cid.equals("")){
-				//用户已登录
-				int customerId = Integer.parseInt(cid);
-				DBUtil db = new DBUtil();
-				String sql = "insert into shopcart(cId,pId,count,isBuy,totalPrice) values(?,?,?,?,?)";
-				Object[] params = {customerId,pId,count,isBuy,totolPrice};
-				int n = db.doUpdate(sql, params);
-				if(n == 1){
-					//添加购物车成功
-					msg = "添加购物车成功,3秒后自动跳转到商品页面.";
+		try {
+            //提示信息的页面
+            String redirectPage = "info.jsp";
+            //自动跳转目录
+            String autoReturnPage = "";
+            //返回的提示信息
+            String msg = "";
+            //获取session
+            HttpSession session = request.getSession();
+            Customer c = (Customer) session.getAttribute("customer");
+            if (c == null) {
+                //未登录;
+                msg = "您还没有登录沃,请登录后再添加购物车吧.";
+                autoReturnPage = "login.jsp";
+            } else {
+                //用户已登录
+                int cid = c.getId();
+                int pId = Integer.parseInt(request.getParameter("pId"));
+                int count = Integer.parseInt(request.getParameter("count"));
+                String isBuy = request.getParameter("isBuy");
 
-				}else {
-					//添加购物车失败
+                //计算总价
+                BigDecimal totolPrice;
+                BigDecimal unitPrice;//商品单价
+                ProductDao pd = new ProductDao();
+                Product p = new Product();
+                p = pd.find(pId);
+                unitPrice = p.getiPrice();
+                totolPrice = unitPrice.multiply(new BigDecimal(Integer.valueOf(count)));
+                //将porduct设置到session中
+                request.getSession().setAttribute("product", p);
+
+                DBUtil db = new DBUtil();
+                String sql = "insert into shopcart(cId,pId,count,isBuy,totalPrice) values(?,?,?,?,?)";
+                Object[] params = {cid, pId, count, isBuy, totolPrice};
+                int n = db.doUpdate(sql, params);
+                if (n == 1) {
+                    //添加购物车成功
                     msg = "添加购物车成功,3秒后自动跳转到商品页面.";
-				}
-                autoReturnPage = "product_info.jsp?id="+pId;
-			}
+
+                } else {
+                    //添加购物车失败
+                    msg = "添加购物车成功,3秒后自动跳转到商品页面.";
+                }
+                autoReturnPage = "product_info.jsp?id=" + pId;
+            }
+            
 			session.setAttribute("msg",msg);
 			session.setAttribute("autoReturn",autoReturnPage);
 			response.sendRedirect(redirectPage);
